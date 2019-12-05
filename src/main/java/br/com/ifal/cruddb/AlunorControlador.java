f35244f9900b041299cac9a67aa3302fac86a565
josyoliveira <joseane.olliveira52@gmail.com>
2019-12-04 23:11:35 -0300

    excluindo dados do Banco

--- src/main/java/br/com/ifal/cruddb/AlunorControlador.java
+++ src/main/java/br/com/ifal/cruddb/AlunorControlador.java
@@ -1,6 +1,7 @@
 package br.com.ifal.cruddb;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,7 +32,12 @@ public class AlunorControlador {
         return resposta;
     }
 
-    
+    @RequestMapping("/aluno/excluir/{matricula}")
+    public ModelAndView excluir(@PathVariable(name = "matricula") long matricula){
+        ar.deleteById(matricula);
+        return new ModelAndView("redirect:/aluno/listar");
+
+    }
 
 
     
